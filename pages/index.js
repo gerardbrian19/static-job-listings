75d3fd63cd06c7ae81a0490da149c1b9ce44f549
GGueco <43228146+gerardbrian19@users.noreply.github.com>
2020-07-06 12:31:52 +0800

    added attribution

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,65 @@
+import React, {Component} from 'react'
 import Head from 'next/head'
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+export default class Index extends Component {
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+    render() {
+        return (
+            <div id="App">
+                <Head>
+                    <title>Frontend Mentor | static-job-listings</title>
+                    <link rel="icon" type="image/png" sizes="32x32" href="/static/images/favicon-32x32.png"/>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+                </Head>
+                <main id="main-content">
+                    <header className="header">
+                        <div className="header__head"></div>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+                        <ul className="header__list">
+                            <ul className="header__filter">
+                                <div className="filters">
+                                    <p className="filter-category">Frontend
+                                        <button className="delete-category">
+                                            <span>&times;</span>
+                                        </button>
+                                    </p>
+                                </div>
+                                <button className="clear">Clear</button>
+                            </ul>
+                        </ul>
+                        <ul className="joblist">
+                            <li className="jobpost">
+                                <img src="/static/images/photosnap.svg" alt="job photo" className="jobpost__img"/>
+                                <div className="jobpost__details">
+                                    <div className="jobpost__status">
+                                        <h3 className="jobpost__company">Photosnap</h3>
+                                        <span className="new">NEW</span>
+                                        <span className="featured">FEATURED</span>
+                                    </div>
+                                    <h2 className="jobpost__role">Senior Frontend Developer</h2>
+                                    <div className="jobpost__date">
+                                        <span className="jobpost__time">1d ago</span>
+                                        <div className="dot"></div>
+                                        <span className="jobpost__time">Full Time</span>
+                                        <div className="dot"></div>
+                                        <span className="jobpost__time">USA Only</span>
+                                    </div>
+                                </div>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+                                <div className="jobpost__categories">
+                                    <button className="category-btn">Frontend</button>
+                                    <button className="category-btn">Senior</button>
+                                    <button className="category-btn">HTML</button>
+                                    <button className="category-btn">CSS</button>
+                                    <button className="category-btn">Javascript</button>
+                                </div>
+                            </li>
+                        </ul>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+                    </header>
+                </main>
+            </div>
+        )
+    }
 }
